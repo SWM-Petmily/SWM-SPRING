4ab5896f2483556c78926e08b3571eb38c91e8b4
kwongio <rldh11111@naver.com>
2023-06-22 17:38:11 +0900

    [feat] : OpenFeign, Security, QueryDsl 설정 추가

---
+++ src/main/java/com/ddungja/app/global/config/OpenFeignConfig.java
@@ -0,0 +1,13 @@
+package com.ddungja.app.global.config;
+
+import feign.Client;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenFeignConfig {
+    @Bean
+    public Client feignClient() {
+        return new Client.Default(null, null);
+    }
+}
---
+++ src/main/java/com/ddungja/app/global/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.ddungja.app.global.config;
+
+import com.ddungja.app.global.CustomResponse;
+import com.ddungja.app.global.jwt.JwtAuthorizationFilter;
+import com.ddungja.app.global.jwt.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class SecurityConfig {
+    private final JwtProvider jwtProvider;
+
+    @RequiredArgsConstructor
+    public static class CustomSecurityFilterManager extends AbstractHttpConfigurer<CustomSecurityFilterManager, HttpSecurity> {
+        private final JwtProvider jwtProvider;
+        @Override
+        public void configure(HttpSecurity http) {
+            AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
+            http.addFilter(new JwtAuthorizationFilter(authenticationManager, jwtProvider));
+        }
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.httpBasic(AbstractHttpConfigurer::disable);
+        http.formLogin(AbstractHttpConfigurer::disable);
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.headers(httpSecurityHeadersConfigurer -> httpSecurityHeadersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+        http.apply(new CustomSecurityFilterManager(jwtProvider));
+        http.exceptionHandling(configurer -> configurer.authenticationEntryPoint((request, response, accessDeniedException) -> CustomResponse.unAuthentication(response, "로그인이 필요합니다.")));
+        http.exceptionHandling(configurer -> configurer.accessDeniedHandler((request, response, accessDeniedException) -> CustomResponse.forbidden(response)));
+        http.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
+        return http.build();
+    }
+}
