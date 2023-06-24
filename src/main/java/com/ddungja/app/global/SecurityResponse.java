d745017e4062b7f0baf179e0ca4f1d2cab352e00
kwongio <rldh11111@naver.com>
2023-06-24 12:57:43 +0900

    [Feat] : ExceptionCode 추가

---
+++ src/main/java/com/ddungja/app/global/SecurityResponse.java
@@ -0,0 +1,32 @@
+package com.ddungja.app.global;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.io.IOException;
+
+public class SecurityResponse {
+
+    public static void unAuthentication(HttpServletResponse response) throws IOException {
+        String result = getResponse(ExceptionCode.UN_AUTHENTICATION);
+        response.setContentType("application/json; charset=utf-8");
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.getWriter().println(result);
+    }
+
+    public static void forbidden(HttpServletResponse response) throws IOException {
+        String result = getResponse(ExceptionCode.FORBIDDEN);
+        response.setContentType("application/json; charset=utf-8");
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.getWriter().println(result);
+    }
+    
+
+    private static String getResponse(ExceptionCode exceptionCode) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(exceptionCode);
+    }
+}
