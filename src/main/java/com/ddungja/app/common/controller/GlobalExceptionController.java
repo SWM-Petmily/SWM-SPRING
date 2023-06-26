1c32cd3b94d81abb0ddd8715ad04383d8f61b401
kwongio <rldh11111@naver.com>
2023-06-26 18:35:08 +0900

    [Feat] : CustomException 추가

--- src/main/java/com/ddungja/app/common/controller/GlobalExceptionController.java
+++ src/main/java/com/ddungja/app/common/controller/GlobalExceptionController.java
@@ -1,8 +1,33 @@
 package com.ddungja.app.common.controller;
 
 
+import com.ddungja.app.common.domain.exception.CustomException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionController {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<?> exceptionHandler(CustomException exception) {
+        return ResponseEntity.status(exception.getExceptionCode().getStatus()).body(exception.getExceptionCode());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> validation(MethodArgumentNotValidException e) {
+        List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
+        Map<String, String> map = new HashMap<>();
+        for (FieldError fieldError : fieldErrors) {
+            map.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return ResponseEntity.badRequest().body(map);
+    }
+
 }
