9deedf6a545c6b9b129f107b1afa6d065d5d327f
kwongio <rldh11111@naver.com>
2023-06-27 18:52:23 +0900

    [style]: 패키지 삭제

--- src/main/java/com/ddungja/app/users/apply/infrastructure/ApplyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ddungja.app.users.apply.infrastructure;
-
-import com.ddungja.app.users.apply.domain.Apply;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApplyRepository extends JpaRepository<Apply, Long> {
-}
