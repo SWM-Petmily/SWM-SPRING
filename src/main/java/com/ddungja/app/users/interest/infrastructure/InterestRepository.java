42c5a0842c1e76b7ea0975a12da5fab0371d8829
kwongio <rldh11111@naver.com>
2023-06-27 18:53:03 +0900

    [style]: 패키지 삭제

--- src/main/java/com/ddungja/app/users/interest/infrastructure/InterestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ddungja.app.users.interest.infrastructure;
-
-import com.ddungja.app.users.interest.domain.Interest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InterestRepository extends JpaRepository<Interest, Long> {
-}
