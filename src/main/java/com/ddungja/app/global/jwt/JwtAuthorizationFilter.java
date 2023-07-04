2ada98db3df812448bdc0d260cd3c9d0eda3be1d
kwongio <rldh11111@naver.com>
2023-07-04 20:58:28 +0900

    [Feat]: 개발환경 debug 로그 보기 설정 추가

--- src/main/java/com/ddungja/app/global/jwt/JwtAuthorizationFilter.java
+++ src/main/java/com/ddungja/app/global/jwt/JwtAuthorizationFilter.java
@@ -29,7 +29,6 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
-        log.debug("jwt 인가 필터");
         String accessToken = resolveToken(request);
         log.debug("accessToken : {}", accessToken);
         if (StringUtils.hasText(accessToken) && jwtProvider.validateAccessToken(accessToken)) {
