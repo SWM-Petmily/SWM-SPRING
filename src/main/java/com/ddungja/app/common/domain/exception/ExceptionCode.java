4eedb762a4d547527677ca4a61e55371de101678
kwongio <rldh11111@naver.com>
2023-07-04 20:58:49 +0900

    [Refactor]: ExceptionCode 수정

--- src/main/java/com/ddungja/app/common/domain/exception/ExceptionCode.java
+++ src/main/java/com/ddungja/app/common/domain/exception/ExceptionCode.java
@@ -7,15 +7,10 @@ import org.springframework.http.HttpStatus;
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 @Getter
 public enum ExceptionCode {
-    UN_AUTHENTICATION(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED, "로그인이 필요합니다."),
-    FORBIDDEN(HttpStatus.FORBIDDEN.value(), HttpStatus.FORBIDDEN, "권한이 없습니다."),
-    NOT_FOUND(HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND, "찾을 수 없습니다."),
-    BAD_REQUEST(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR, "서버에 오류가 발생했습니다."),
-
-
-    //profile
-    PROFILE_IMAGE_NOT_FOUND(1001, HttpStatus.NOT_FOUND, "프로필 이미지를 찾을 수 없습니다."),
+    //권한 관련
+    UN_AUTHENTICATION(0001, HttpStatus.UNAUTHORIZED, "로그인이 필요합니다."),
+    FORBIDDEN(0002, HttpStatus.FORBIDDEN, "권한이 없습니다."),
+    REFRESH_TOKEN_NOT_FOUND(0003, HttpStatus.NOT_FOUND, "리프레시 토큰을 찾을 수 없습니다." ),
 
 
     //user
@@ -25,10 +20,13 @@ public enum ExceptionCode {
     //profile
     PROFILE_NOT_FOUND(3001, HttpStatus.NOT_FOUND, "프로필을 찾을 수 없습니다."),
     PROFILE_ALREADY_EXISTS(3002, HttpStatus.BAD_REQUEST, "프로필이 이미 존재합니다."),
+    PROFILE_IMAGE_NOT_FOUND(3003, HttpStatus.NOT_FOUND, "프로필 이미지를 찾을 수 없습니다."),
+
 
 
     //experience
     EXPERIENCE_NOT_FOUND(4001, HttpStatus.NOT_FOUND, "반려동물 경험을 찾을 수 없습니다." );
+
 
 
 
