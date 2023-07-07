4511676c07f6d200e7433e9d1e4d8f30ee6fcc9f
kwongio <rldh11111@naver.com>
2023-07-07 14:24:06 +0900

    [Refactor]:  배포 설정

---
+++ src/main/java/com/ddungja/petmily/common/controller/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.ddungja.petmily.common.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/")
+    public ResponseEntity<?> health(){
+        return ResponseEntity.ok("good");
+    }
+}
