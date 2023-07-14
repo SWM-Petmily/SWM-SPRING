bd9549bc479d51a3434ca9fbfb2213f60cf74567
aran-kim <70698666+aran-kim@users.noreply.github.com>
2023-07-14 17:02:11 +0900

    [Refactor]: 구조 변경 및 페이지네이션
    subcategory 구조 수정
    like 페이지네이션 및 postStatusType 추가
    registration 구조 수정

--- src/main/java/com/ddungja/petmily/common/config/QuerydslConfig.java
+++ src/main/java/com/ddungja/petmily/common/config/QuerydslConfig.java
@@ -9,9 +9,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @RequiredArgsConstructor
 public class QuerydslConfig {
-
-    private final EntityManager em;
-
     @Bean
     public JPAQueryFactory jpaQueryFactory(EntityManager em) {
         return new JPAQueryFactory(em);
