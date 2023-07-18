7a13d9253f0fe284433194d3e4e9ae2fc59c5cc4
kwongio <rldh11111@naver.com>
2023-07-18 14:26:46 +0900

    [Refactor] data.sql 데이터 추가

--- src/test/java/com/ddungja/petmily/AppApplicationTests.java
+++ src/test/java/com/ddungja/petmily/AppApplicationTests.java
@@ -1,18 +1,30 @@
 package com.ddungja.petmily;
 
+import com.ddungja.petmily.apply.domain.ApprovalType;
+import com.ddungja.petmily.post.domain.type.PostStatusType;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 
 class AppApplicationTests {
 
-    
-    @Test
-    public void test(){
-        LocalDate start = LocalDate.of(2022, 7, 1);
 
-        LocalDate end = LocalDate.now();
-        System.out.println(ChronoUnit.MONTHS.between(start, end));
+    @DisplayName("apply 값 생성")
+    @Test
+    public void apply() {
+        int num = 1;
+        for (ApprovalType value : ApprovalType.values()) {
+            for (int i = 0; i < 50; i++) {
+                System.out.println("(" + num + ", 2, 1, 1, '" + value + "', now(), now(), '직장인', '집', 3, 'www.naver.com', '서울', true,  'https://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcREj22c-wMNL5IDmU99v8G7voUl17Yxm0JJqMLqttdPT4DnaB99zqVK7HWiNzjP3aZnzCEf-ikAqb2yiDk', '잘 키울 수 있음'),");
+                num++;
+            }
+        }
+        num = 1;
+        for (PostStatusType value : PostStatusType.values()) {
+            for (int i = 0; i < 50; i++) {
+                System.out.println("('입양자', '장점','평균비용','2022-03',now(),'단점','MALE',1,'100000','테스트','YES','이유','서울',1,'"+value+"',1,'https://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcREj22c-wMNL5IDmU99v8G7voUl17Yxm0JJqMLqttdPT4DnaB99zqVK7HWiNzjP3aZnzCEf-ikAqb2yiDk',now(),1,1,"+num+"),");
+                num++;
+            }
+        }
+
     }
 }
