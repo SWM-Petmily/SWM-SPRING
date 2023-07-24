ba63ef54b3745f25952bf6d3b9f189945b0ed6b3
kwongio <rldh11111@naver.com>
2023-07-25 02:39:33 +0900

    [Refactor] swagger server 옵션 localhost 추가

--- src/main/java/com/ddungja/petmily/AppApplication.java
+++ src/main/java/com/ddungja/petmily/AppApplication.java
@@ -10,7 +10,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableFeignClients
 @EnableJpaAuditing
-@OpenAPIDefinition(servers = {@Server(url = "https://www.petmily.site", description = "production server url")})
+@OpenAPIDefinition(servers = {@Server(url = "https://www.petmily.site",  description = "production server url")
+    , @Server(url = "http://localhost:8080", description = "dev server url")})
 public class AppApplication {
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
