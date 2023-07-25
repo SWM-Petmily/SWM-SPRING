26d5f37a1d8375c6bc285f9f1cc3f3fd0c27969f
kwongio <rldh11111@naver.com>
2023-07-25 21:25:04 +0900

    [Refactor]: kakao login 로그 추가

--- src/main/java/com/ddungja/petmily/user/service/UserService.java
+++ src/main/java/com/ddungja/petmily/user/service/UserService.java
@@ -9,6 +9,7 @@ import com.ddungja.petmily.user.domain.User;
 import com.ddungja.petmily.user.domain.request.UserUpdateRequest;
 import com.ddungja.petmily.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,13 +17,14 @@ import static com.ddungja.petmily.common.domain.exception.ExceptionCode.USER_NOT
 
 @Service
 @RequiredArgsConstructor
-
+@Slf4j
 public class UserService {
 
     private final UserRepository userRepository;
 
     @Transactional
     public User login(KakaoProfile kakaoProfile) {
+        log.info("kakao login email = {}", kakaoProfile.getEmail());
         return userRepository.findByEmail(kakaoProfile.getEmail()).orElseGet(() -> userRepository.save(User.builder().email(kakaoProfile.getEmail()).provider(ProviderType.KAKAO).build()));
     }
 
