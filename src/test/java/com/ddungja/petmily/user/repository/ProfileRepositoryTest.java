71a7df715e1e25388be43ab4ed99b3975d77e836
kwongio <rldh11111@naver.com>
2023-08-08 17:29:12 +0900

    [Feat] 서브모듈 추가

--- src/test/java/com/ddungja/petmily/user/repository/ProfileRepositoryTest.java
+++ src/test/java/com/ddungja/petmily/user/repository/ProfileRepositoryTest.java
@@ -1,29 +1,23 @@
 package com.ddungja.petmily.user.repository;
 
-import com.ddungja.petmily.user.domain.profile.Profile;
-import jakarta.persistence.EntityManager;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
 
 @SpringBootTest
 class ProfileRepositoryTest {
 
-    @Autowired
-    private ProfileRepository profileRepository;
-    @Autowired
-    EntityManager entityManager;
-    
-    @Test
-    public void test1() {
-        //given
-
-        //when
-        //then
-        List<Profile> resultList1 = entityManager.createQuery("select p from Profile p  join fetch p.experiences e ", Profile.class).getResultList();
-        System.out.println("resultList = " + resultList1.size());
-    }
+//    @Autowired
+//    private ProfileRepository profileRepository;
+//    @Autowired
+//    EntityManager entityManager;
+//
+//    @Test
+//    public void test1() {
+//        //given
+//
+//        //when
+//        //then
+//        List<Profile> resultList1 = entityManager.createQuery("select p from Profile p  join fetch p.experiences e ", Profile.class).getResultList();
+//        System.out.println("resultList = " + resultList1.size());
+//    }
 
 }
