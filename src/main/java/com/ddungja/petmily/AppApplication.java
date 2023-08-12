04d416ffb0a626800d0bf594ab72cadc955909cc
kwongio <rldh11111@naver.com>
2023-08-12 23:00:39 +0900

    [Refactor] config 분리

--- src/main/java/com/ddungja/petmily/AppApplication.java
+++ src/main/java/com/ddungja/petmily/AppApplication.java
@@ -1,17 +1,9 @@
 package com.ddungja.petmily;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
-@EnableFeignClients
-@OpenAPIDefinition(servers = {@Server(url = "http://localhost:8080", description = "dev server url")
-        , @Server(url = "https://www.petmily.site",  description = "production server url")})
 public class AppApplication {
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
--- src/main/java/com/ddungja/petmily/common/config/OpenApiConfig.java
+++ src/main/java/com/ddungja/petmily/common/config/OpenApiConfig.java
@@ -1,5 +1,7 @@
 package com.ddungja.petmily.common.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -8,8 +10,9 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@OpenAPIDefinition(servers = {@Server(url = "http://localhost:8080", description = "dev server url")
+        , @Server(url = "https://www.petmily.site",  description = "production server url")})
 @Configuration
-
 public class OpenApiConfig {
     @Bean
     public OpenAPI openAPI() {
