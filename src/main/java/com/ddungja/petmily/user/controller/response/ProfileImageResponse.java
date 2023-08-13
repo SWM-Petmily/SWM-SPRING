6eb9f7a64e48706f1eef70af0121ff7bdd0dfc9a
kwongio <rldh11111@naver.com>
2023-08-13 18:03:00 +0900

    [Refactor] ProfileImageResponse id추가

--- src/main/java/com/ddungja/petmily/user/controller/response/ProfileImageResponse.java
+++ src/main/java/com/ddungja/petmily/user/controller/response/ProfileImageResponse.java
@@ -7,13 +7,16 @@ import lombok.Getter;
 
 @Getter
 public class ProfileImageResponse {
+    private final Long id;
     private final String url;
     @Builder
-    public ProfileImageResponse(String url) {
+    public ProfileImageResponse(Long id, String url) {
+        this.id = id;
         this.url = url;
     }
     public static ProfileImageResponse from(ProfileImage profileImage) {
         return ProfileImageResponse.builder()
+                .id(profileImage.getId())
                 .url(profileImage.getUrl())
                 .build();
     }
