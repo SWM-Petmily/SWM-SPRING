18f162c797f75292b4850bd35b604b71b5ad5fa2
kwongio <rldh11111@naver.com>
2023-09-26 22:29:19 +0900

    [Feat] 웹 소켓 채팅방 여러개

--- src/main/java/com/ddungja/petmily/chat/WebSockChatHandler.java
+++ src/main/java/com/ddungja/petmily/chat/WebSockChatHandler.java
@@ -2,6 +2,8 @@ package com.ddungja.petmily.chat;
 
 // import 생략....
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
@@ -9,29 +11,32 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class WebSockChatHandler extends TextWebSocketHandler {
 
-    private final static List<WebSocketSession> sessions = new ArrayList<>();
+    private final ObjectMapper objectMapper;
+    private final ChatService chatService;
+
+//    private final static List<WebSocketSession> sessions = new ArrayList<>();
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String payload = message.getPayload();
         log.info("payload {}", payload);
-        TextMessage textMessage = new TextMessage(session.toString());
-        for(WebSocketSession sess: sessions) {
-            sess.sendMessage(textMessage);
-            sess.sendMessage(message);
-        }
+//        TextMessage textMessage = new TextMessage(session.toString());
+//        for(WebSocketSession sess: sessions) {
+//            sess.sendMessage(textMessage);
+//            sess.sendMessage(message);
+//        }
+        ChatMessage chatMessage = objectMapper.readValue(payload, ChatMessage.class);
+        ChatRoom room = chatService.findRoomById(chatMessage.getRoomId());
+        room.handleActions(session, chatMessage, chatService);
     }
 
     /* Client가 접속 시 호출되는 메서드 */
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        sessions.add(session);
         log.info(session + " 클라이언트 접속");
     }
 
@@ -40,6 +45,5 @@ public class WebSockChatHandler extends TextWebSocketHandler {
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         log.info(session + " 클라이언트 접속 해제");
-        sessions.remove(session);
     }
 }
